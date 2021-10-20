0850192a581aa8e44bea320a46950972f085ae98
shngmsw <mose1021@gmail.com>
2021-10-20 20:42:49 +0900

    code format

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
-const fs = require('fs');
+const fs = require("fs");
 const { allowedNodeEnvironmentFlags } = require("process");
 // const guildID = '802256309302460486' // test server
 const guildID = "669268347736686612"; // cosmos server
@@ -9,11 +9,12 @@ const client = new Discord.Client();
 const invites = {};
 
 // A pretty useful method to create a delay without blocking the whole script.
-const wait = require('util').promisify(setTimeout);
+const wait = require("util").promisify(setTimeout);
 
-client.on('ready', async () => {
+client.on("ready", async () => {
   // "ready" isn't really ready. We need to wait a spell.
   await wait(1000);
+  console.log(`Logged in as ${client.user.tag}!`);
 
   client.guilds.cache.forEach(g => {
     g.fetchInvites().then(guildInvites => {
@@ -22,57 +23,56 @@ client.on('ready', async () => {
   });
 });
 
-client.on('guildMemberAdd', member => {
+client.on("guildMemberAdd", member => {
   // To compare, we need to load the current invite list.
   member.guild.fetchInvites().then(guildInvites => {
-    
     // This is the *existing* invites for the guild.
     const ei = invites[member.guild.id];
-    
+
     invites[member.guild.id] = guildInvites;
     // Look through the invites, find the one for which the uses went up.
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     if (invite !== null) {
-      addRole(member, invite)
+      addRole(member, invite);
     }
   });
 });
 
 const prefix = "~";
-client.on("message", function(message) { 
+client.on("message", function(message) {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
   if (!message.member.hasPermission("ADMINISTRATOR")) return;
 
   const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
+  const args = commandBody.split(" ");
   const command = args.shift().toLowerCase();
 
-
-  switch(command){
-    case('add'):
-      add(message, args)
+  switch (command) {
+    case "add":
+      add(message, args);
       break;
-    case('remove'):
-      remove(message, args)
-      break
-    case('list'):
-      list(message)
+    case "remove":
+      remove(message, args);
+      break;
+    case "list":
+      list(message);
       break;
     default:
-      message.reply(`command doesn't exist`)
+      message.reply(`command doesn't exist`);
   }
 
   if (command === "ping") {
     const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);              
-  }   
-});    
+    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+  }
+});
 
 function addRole(member, invite) {
-  let rawdata = fs.readFileSync('invites.json');
+  let rawdata = fs.readFileSync("invites.json");
   let _invites = JSON.parse(rawdata);
-  const {roleID} = _invites[invite.code]
+
+  const { roleID } = _invites[invite.code];
   if (roleID) {
     var role = member.guild.roles.cache.find(role => role.id === roleID);
     member.roles.add(role);
@@ -80,44 +80,48 @@ function addRole(member, invite) {
 }
 
 function list(message) {
-
-  let rawdata = fs.readFileSync('invites.json');
+  let rawdata = fs.readFileSync("invites.json");
   let _invites = JSON.parse(rawdata);
-  message.reply(`\`\`\`\n${JSON.stringify(_invites, null, 2)}\n\`\`\``)
+  message.reply(`\`\`\`\n${JSON.stringify(_invites, null, 2)}\n\`\`\``);
 }
 
 function add(message, args) {
-  let rawdata = fs.readFileSync('invites.json');
+  let rawdata = fs.readFileSync("invites.json");
   let _invites = JSON.parse(rawdata);
 
   if (args.length !== 2) {
-    message.reply(`not enough arguments`)
-    return
+    message.reply(`not enough arguments`);
+    return;
   }
-  const base = 'https://discord.gg/'
+  const base = "https://discord.gg/";
   if (args[0].substring(0, base.length) !== base) {
     message.reply(`missing link starting with \`${base}\``);
-    return
+    return;
   }
-  const inviteCode = args[0].substring(base.length)
-  const roleprefix = '<@&'
+  const inviteCode = args[0].substring(base.length);
+  const roleprefix = "<@&";
   if (args[1].substring(0, roleprefix.length) !== roleprefix) {
     message.reply(`invalid role`);
-    return
+    return;
   }
-  const roleID = args[1].substr(roleprefix.length, args[1].length - roleprefix.length - 1)
+  const roleID = args[1].substr(
+    roleprefix.length,
+    args[1].length - roleprefix.length - 1
+  );
   let role = message.guild.roles.cache.find(x => x.id === roleID);
 
   if (typeof role === undefined) {
-      message.reply(`invalid role`);
-      return
+    message.reply(`invalid role`);
+    return;
   }
 
-  _invites[inviteCode] = {roleID, name: role.name}
+  _invites[inviteCode] = { roleID, name: role.name };
 
   let data = JSON.stringify(_invites, null, 2);
-  fs.writeFileSync('invites.json', data);
-  message.reply(`role @${role.name} added to invite link \`${base + inviteCode}\``)
+  fs.writeFileSync("invites.json", data);
+  message.reply(
+    `role @${role.name} added to invite link \`${base + inviteCode}\``
+  );
 }
 
 client.login(config.BOT_TOKEN);
